970b94c4c8e705d4c081ff492d37d35461c64f90
dancdx <1049555787@qq.com>
2018-02-07 18:05:44 +0800

    feat(goods): 查询全部状态修改

--- src/pages/Goods/index.js
+++ src/pages/Goods/index.js
@@ -14,7 +14,7 @@ class GoodsList extends Component {
       options: [
         { value: 0, label: '正常' },
         { value: 1, label: '已下架' },
-        { value: '', label: '全部' }
+        { value: 3, label: '全部' }
       ],
       status: ''
     }
